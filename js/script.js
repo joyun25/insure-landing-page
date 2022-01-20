4fc94b8ea8250e6581113a0fb7359db60cb44ca7
joyun25 <88151037+joyun25@users.noreply.github.com>
2022-01-20 18:55:58 +0800

    improve mobile menu animation

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ const menuList = document.querySelector('.menu_list');
 const menuIcon = document.querySelector('.menu_icon');
 
 menuIcon.addEventListener('click', () => {
-    if (menuList.classList.contains('d-block')) {
-        menuList.classList.remove('d-block')
+    if (menuList.classList.contains('menuShow')) {
+        menuList.classList.remove('menuShow')
         menuIcon.innerHTML = `<img src="images/icon-hamburger.svg" alt="menu icon">`;
     }else{
-        menuList.classList.add('d-block')
+        menuList.classList.add('menuShow')
         menuIcon.innerHTML = `<img src="images/icon-close.svg" alt="menu close icon">`;
     }
 })
